f13efc490681bc4d81e385d1e700f089a69b6ae9
German Araujo <germanaraujo@Germans-MacBook-Pro.local>
2025-07-26 16:19:15 -0300

    momo2

--- src/main/java/uy/com/bay/utiles/data/AlchemerContact.java
+++ src/main/java/uy/com/bay/utiles/data/AlchemerContact.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "alchemer_survey_response")
+@Table(name = "alchemer_contact")
 public class AlchemerContact extends AbstractEntity {
 
     @JsonProperty("Email")
