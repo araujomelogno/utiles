16639102ff446421f3a01e57a4aec09c45edfdf6
German Araujo <germanaraujo@Germans-MacBook-Pro.local>
2025-07-27 21:46:51 -0300

    mkm2

--- src/main/java/uy/com/bay/utiles/data/repository/AlchemerSurveyResponseRepository.java
+++ src/main/java/uy/com/bay/utiles/data/repository/AlchemerSurveyResponseRepository.java
@@ -1,12 +1,15 @@
 package uy.com.bay.utiles.data.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import uy.com.bay.utiles.data.AlchemerSurveyResponse;
+import java.util.List;
 
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uy.com.bay.utiles.data.AlchemerSurveyResponse;
 
 public interface AlchemerSurveyResponseRepository extends JpaRepository<AlchemerSurveyResponse, Long> {
 
-    Optional<AlchemerSurveyResponse> findByDataResponseIdAndDataSurveyId(Long responseId, Integer surveyId);
+
+    List<AlchemerSurveyResponse> findByDataResponseIdAndDataSurveyId(Long responseId, Integer surveyId);
+
 
 }
