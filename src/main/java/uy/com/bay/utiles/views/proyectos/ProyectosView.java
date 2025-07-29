19ab80786848b84e89311b344ee428ba2762fa6f
German Araujo <germanaraujo@Germans-MacBook-Pro.local>
2025-07-29 17:58:42 -0300

    imi2

--- src/main/java/uy/com/bay/utiles/views/proyectos/ProyectosView.java
+++ src/main/java/uy/com/bay/utiles/views/proyectos/ProyectosView.java
@@ -326,7 +326,7 @@ public class ProyectosView extends Div implements BeforeEnterObserver {
 
     private void clearForm() {
         populateForm(null);
-    }
+    } 
 
     private void populateForm(Proyecto value) {
         this.proyecto = value;
