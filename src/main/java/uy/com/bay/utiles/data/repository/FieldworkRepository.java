66848722427da860f223af4317d1f1dec0dc4b99
German Araujo <germanaraujo@Germans-MacBook-Pro.local>
2025-09-03 18:32:30 -0300

    mimi

--- src/main/java/uy/com/bay/utiles/data/repository/FieldworkRepository.java
+++ src/main/java/uy/com/bay/utiles/data/repository/FieldworkRepository.java
@@ -4,8 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import uy.com.bay.utiles.data.Fieldwork;
-import uy.com.bay.utiles.data.Study;
 
 public interface FieldworkRepository extends JpaRepository<Fieldwork, Long>, JpaSpecificationExecutor<Fieldwork> {
 	Optional<Fieldwork> findByAlchemerId(String alchemerId);
