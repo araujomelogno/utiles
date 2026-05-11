f6737a0b3fa18df003f2a180bd3a87643f390057
German <araujomelogno@gmail.com>
2026-05-11 18:45:47 -0300

    moim

--- src/main/java/uy/com/bay/utiles/tasks/OdooProjectSyncTask.java
+++ src/main/java/uy/com/bay/utiles/tasks/OdooProjectSyncTask.java
@@ -28,7 +28,7 @@ public class OdooProjectSyncTask {
 	// hour
 	// For testing, you might use a more frequent cron like "*/30 * * * * ?" (every
 	// 30 seconds)
-	@Scheduled(cron = "0 */50 * * * *")
+	@Scheduled(cron = "* * 5 * * *")
 	public void syncOdooProjects() {
 		System.out.println("Starting Odoo Project Sync Task...");
 
